17b815904e778aa43f469d222052f1b87c457094
BabyBlue52 <chris.hennemann256@gmail.com>
2023-07-16 17:18:58 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HyypCalendar from "./components/HyypCalendar.vue";
+
+</script>
+
+<template>
+
+  <HyypCalendar/>
+</template>
+
+<style scoped >
+
+</style>
---
+++ src/components/HyypCalendar.vue
@@ -0,0 +1,39 @@
+<template>
+      <!-- Toggle View -->
+      <div style="display:flex">
+        <div style="margin-right: 10px">
+            <input type="radio" value="Week" name="calendar"/>
+            <label>Week</label>
+        </div>
+        <div>
+            <input type="radio" value="Day" name="calendar"/>
+            <label>Day</label>
+        </div>
+    </div>
+    <!-- Calendar Component -->
+    <div class="hyyp-calendar-wrapper">
+        <FullCalendar :options="calendarOptions" class="hyyp-calendar" />
+    </div>
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [dayGridPlugin, interactionPlugin],
+                initialView: 'dayGridWeek'
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" src="../assets/main.scss"/>
